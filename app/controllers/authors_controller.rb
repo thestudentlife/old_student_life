945bf675ed82c0fdf485322514a4e508bda7b25c
Michael Maltese <mike@mikemaltese.com>
2011-03-03 10:33:39 -0800

    Add author to article

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,18 @@
 class AuthorsController < ApplicationController
+  def new
+    @article = Article.find params[:article_id]
+    @authors = StaffMember.all
+  end
+  def create
+    @article = Article.find params[:article_id]
+    @author = StaffMember.find params[:author_id]
+    
+    if @article.authors << @author
+      redirect_to @article, :notice => 'Author was successfully added'
+    else
+      redirect_to new_article_author_path(@article), :notice => 'Author was not added'
+    end
+  end
   def destroy
     @article = Article.find params[:article_id]
     @author = @article.authors.find params[:author_id]
