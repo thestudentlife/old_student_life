a929806f1452b0ed92c9bcf7cfade3a99342790f
Michael Maltese <mike@mikemaltese.com>
2011-03-03 12:59:35 -0800

    Revision.latest_published

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -3,6 +3,13 @@ class Revision < ActiveRecord::Base
   belongs_to :author, :class_name => "StaffMember"
   validates_presence_of :body
   
+  def self.latest_published
+    self.group ("article_id"
+    ).where (:published_online => true
+    ).where ("published_online_at < ?", Time.now
+    )
+  end
+  
   def to_s
     body
   end
