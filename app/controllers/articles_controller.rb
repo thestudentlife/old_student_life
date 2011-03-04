cb2fdb683e3591f2ff4fd24912097557b00bb718
Michael Maltese <mike@mikemaltese.com>
2011-03-03 21:25:00 -0800

    FrontPageArticles show up differently

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @front_page_revisions = Revision.latest_published_on_front_page.include(:article)
+    @front_page_revisions = Revision.latest_published_on_front_page.includes(:article)
     @revisions = Revision.latest_published_not_on_front_page.includes(:article)
   end
   def show
--- app/controllers/workflow_controller.rb
+++ app/controllers/workflow_controller.rb
@@ -1,4 +1,6 @@
 class WorkflowController < ApplicationController
+  before_filter :require_user
+
   def index
   end
 end
