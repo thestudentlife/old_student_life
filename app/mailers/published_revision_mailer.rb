f5e0d5c30447d0ae6dc12a7197f88b626bf22475
Michael Maltese <mike@mikemaltese.com>
2011-03-09 16:20:06 -0800

    Mailer converts HTML to InCopy

--- app/mailers/published_revision_mailer.rb
+++ app/mailers/published_revision_mailer.rb
@@ -2,7 +2,7 @@ class PublishedRevisionMailer < ActionMailer::Base
   default :from => "from@example.com"
   
   def revision_email (revision)
-    attachments["#{revision.title}.html"] = revision.body
+    attachments["#{revision.title}.incx"] = InCopy.html_to_incopy(revision.body)
     mail (:to => "mchtly@gmail.com", :subject => revision.title )
   end
 end
