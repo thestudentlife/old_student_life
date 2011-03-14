b0edbd241d3f6bdbf91d914d41ac3576a1668e6c
Michael Maltese <mike@mikemaltese.com>
2011-03-14 13:24:28 -0700

    workflow/editors controller using respond_with

--- app/controllers/workflow/editors_controller.rb
+++ app/controllers/workflow/editors_controller.rb
@@ -1,34 +1,26 @@
 class Workflow::EditorsController < WorkflowController
+  respond_to :html
   
-  before_filter :require_user, :find_section
-  before_filter {current_user.can_edit_sections!}
+  before_filter :require_user
+  before_filter { current_user.can_edit_sections! }
+  before_filter { @section = Section.find params[:section_id]}
   
   def new
-    @editors = User.all
+    respond_with :workflow, @editors = User.all
   end
   def create
     @editor = User.find params[:editor_id]
     
     if @section.editors.include? @editor
-      redirect_to new_workflow_section_editor_path(@section), :notice => "Editor already exists"
+      render :new, :notice => "Editor already exists"
     elsif @section.editors << @editor
-      redirect_to workflow_sections_path, :notice => 'Editor was successfully added'
+      redirect_to [:workflow, :sections], :notice => 'Editor was successfully added'
     else
-      redirect_to new_workflow_section_editor_path(@section), :notice => 'Editor was not added'
+      render :new, :notice => 'Editor was not added'
     end
   end
   def destroy
-    @editor = @section.editors.find params[:id]
-    
-    if @section.editors.delete @editor
-      redirect_to workflow_sections_path, :notice => 'Editor was successfully removed'
-    else
-      redirect_to workflow_sections_path, :notice => 'Editor was not removed'
-    end
-  end
-  
-  private
-  def find_section
-    @section = Section.find params[:section_id]
+    @section.editors.delete(@section.editors.find params[:id])
+    respond_with :workflow, @section, :location => [:workflow, :sections]
   end
 end
