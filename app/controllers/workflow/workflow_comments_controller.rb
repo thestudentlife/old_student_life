bcbd8f2b1aa3804646caaf1896669e277e79664b
Michael Maltese <mike@mikemaltese.com>
2011-03-14 14:23:58 -0700

    workflow/comments uses respond_with

--- app/controllers/workflow/workflow_comments_controller.rb
+++ app/controllers/workflow/workflow_comments_controller.rb
@@ -1,29 +1,21 @@
 class Workflow::WorkflowCommentsController < WorkflowController
+  respond_to :html
   
-  before_filter :require_user, :find_article
-  before_filter {current_user.can_post_to_article! @article}
+  before_filter :require_user
+  before_filter { @article = Article.find params[:article_id] }
+  before_filter { current_user.can_post_to_article! @article }
   
-  # GET /workflow_comments/new
-  # GET /workflow_comments/new.xml
   def new
-    @workflow_comment = WorkflowComment.new
+    respond_with :workflow, @workflow_comment = WorkflowComment.new
   end
 
   def create
     params[:workflow_comment][:author_id] = current_user.id
-    @workflow_comment = WorkflowComment.new(params[:workflow_comment])
-    @workflow_comment.article = @article
-    @workflow_comment.visible_to_article_author = (@article.open_to_author or @article.workflow_status.open_to_author)
+    @workflow_comment = WorkflowComment.create params[:workflow_comment].merge({
+      :article => @article,
+      :visible_to_article_author => (@article.open_to_author or @article.workflow_status.open_to_author)
+    })
     
-    if @workflow_comment.save
-      redirect_to workflow_article_path(@article), :notice => 'Comment was successfully created.'
-    else
-      render :action => "new"
-    end
-  end
-  
-  private
-  def find_article
-    @article = Article.find params[:article_id]
+    respond_with :workflow, @workflow_comment, :location => [:workflow, @article]
   end
 end
