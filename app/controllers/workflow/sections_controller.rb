00c415ba76015de294f6b69091e3465af7c9ae6d
Michael Maltese <mike@mikemaltese.com>
2011-03-14 14:00:20 -0700

    workflow/sections uses respond_with

--- app/controllers/workflow/sections_controller.rb
+++ app/controllers/workflow/sections_controller.rb
@@ -1,43 +1,33 @@
 class Workflow::SectionsController < WorkflowController
+  respond_to :html
 
   before_filter :require_user
   before_filter {current_user.can_edit_sections!}
 
   def index
-    @sections = Section.all
+    respond_with :workflow, @sections = Section.all
   end
 
   def new
-    @section = Section.new
+    respond_with :workflow, @section = Section.new
   end
 
   def edit
-    @section = Section.find(params[:id])
+    respond_with :workflow, @section = Section.find(params[:id])
   end
 
   def create
-    @section = Section.new(params[:section])
-
-   if @section.save
-      redirect_to(workflow_sections_path, :notice => 'Section was successfully created.')
-    else
-      render :action => "new"
-    end
+    @section = Section.create(params[:section])
+    respond_with :workflow, @section, :location => [:workflow, :sections]
   end
 
   def update
-    @section = Section.find(params[:id])
-
-    if @section.update_attributes(params[:section])
-      redirect_to(workflow_sections_path, :notice => 'Section was successfully updated.')
-    else
-      render :action => "edit"
-    end
+    @section = Section.update(params[:id], params[:section])
+    respond_with :workflow, @section, :location => [:workflow, :sections]
   end
 
   def destroy
     @section = Section.find(params[:id])
-    
     if @section.articles.empty?
       @section.destroy
       flash[:notice] = 'Section was successfully destroyed'
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,8 @@ class Section < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :url
 
+  validates :priority, :numericality => { :only_integer => true }
+
   has_many :articles
   has_and_belongs_to_many :editors, :class_name => "User"
   has_many :subsections
