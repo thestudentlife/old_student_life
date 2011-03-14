17934eaaf240726704287376a1c79f0a6aa277bd
Michael Maltese <mike@mikemaltese.com>
2011-03-14 14:28:09 -0700

    workflow/statuses uses respond_with

--- app/controllers/workflow/workflow_statuses_controller.rb
+++ app/controllers/workflow/workflow_statuses_controller.rb
@@ -1,37 +1,30 @@
 class Workflow::WorkflowStatusesController < WorkflowController
+  respond_to :html
   
   before_filter :require_user
   before_filter {current_user.can_edit_workflow_statuses!}
   
   def index
-    @workflow_statuses = WorkflowStatus.all
+    respond_with :workflow, @workflow_statuses = WorkflowStatus.all
   end
 
   def new
-    @workflow_status = WorkflowStatus.new
+    respond_with :workflow, @workflow_status = WorkflowStatus.new
   end
 
   def edit
-    @workflow_status = WorkflowStatus.find params[:id]
+    respond_with :workflow, @workflow_status = WorkflowStatus.find(params[:id])
   end
 
   def create
-    @workflow_status = WorkflowStatus.new params[:workflow_status]
-
-    if @workflow_status.save
-      redirect_to workflow_statuses_path, :notice => 'Workflow status was successfully created.'
-    else
-      render :action => "new"
-    end
+    respond_with :workflow,
+      @workflow_status= WorkflowStatus.create(params[:workflow_status]),
+      :location => [:workflow, :statuses]
   end
 
   def update
-    @workflow_status = WorkflowStatus.find params[:id]
-
-    if @workflow_status.update_attributes params[:workflow_status]
-      redirect_to workflow_statuses_path, :notice => 'Workflow status was successfully updated.'
-    else
-      render :action => "edit"
-    end
+    respond_with :workflow,
+      @workflow_status = WorkflowStatus.update(params[:id], params[:workflow_status]),
+      :location => [:workflow, :statuses]
   end
 end
