b5bdc74fe4407bd87e6d1a83d2e91f49e58c9124
Michael Maltese <mike@mikemaltese.com>
2011-03-14 11:34:25 -0700

    Moved workflow/authors controller to use responds_with

--- app/controllers/workflow/authors_controller.rb
+++ app/controllers/workflow/authors_controller.rb
@@ -1,44 +1,42 @@
 class Workflow::AuthorsController < WorkflowController
+  respond_to :html
+  
   def index
     @authors = Author.all
+    respond_with(:workflow, @authors)
   end
 
   def show
     @author = Author.find(params[:id])
+    respond_with(:workflow, @author)
   end
 
   def new
     @author = Author.new
+    respond_with(:workflow, @author)
   end
 
   def edit
     @author = Author.find(params[:id])
+    respond_with(:workflow, @author)
   end
 
   def create
     @author = Author.new(params[:author])
-
-    if @author.save
-      redirect_to(workflow_authors_path, :notice => 'Author was successfully created.')
-    else
-      render :action => "new"
-    end
+    flash[:notice] = "Author was successfully created" if @author.save
+    respond_with(:workflow, @author, :location => [:workflow, :authors])
   end
 
   def update
     @author = Author.find(params[:id])
-
-    if @author.update_attributes(params[:author])
-      redirect_to(workflow_authors_path, :notice => 'Author was successfully updated.')
-    else
-      render :action => "edit"
-    end
+    flash[:notice] = "Author was successfully updated" if @author.update_attributes(params[:author])
+    respond_with(:workflow, @author, :location => [:workflow, :authors])
   end
 
   def destroy
     @author = Author.find(params[:id])
     @author.destroy
-
-    redirect_to(workflow_authors_path)
+    flash[:notice] = "Successfully destroyed author"
+    respond_with(:workflow, @author)
   end
 end
