ee8397a74de58119a50229c8398cab1064140d57
Michael Maltese <mike@mikemaltese.com>
2011-03-17 11:53:44 -0700

    WorkflowController catches User::NotAuthorized and makes it a flash[:error]

--- app/controllers/workflow_controller.rb
+++ app/controllers/workflow_controller.rb
@@ -2,4 +2,13 @@ class WorkflowController < ApplicationController
   before_filter :require_user
   responders :flash
   layout 'workflow'
+  
+  rescue_from User::NotAuthorized, :with => :not_authorized
+  
+  private
+  
+  def not_authorized
+    flash[:error] = "You do not have the correct permissions to access #{request.fullpath}"
+    redirect_to workflow_path
+  end
 end
