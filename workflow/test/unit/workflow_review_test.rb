7843052b6c8cb2778527083afc3899bb3664e167
Michael Maltese <mike@mikemaltese.com>
2011-03-18 11:08:04 -0700

    Nicer validator on WorkflowReview

--- workflow/test/unit/workflow_review_test.rb
+++ workflow/test/unit/workflow_review_test.rb
@@ -5,11 +5,22 @@ class WorkflowReviewTest < ActiveSupport::TestCase
     news = Section.create! :name => "news", :priority => 10, :url => "news"
     slot = ReviewSlot.create! :name => "Section Editor"
     
-    a = Article.create! :section => news, :working_name => "smiley 80s"
+    a = Article.create! :section => news, :name => "smiley 80s"
     r1 = WorkflowReview.create! :article => a, :review_slot => slot
     
     r2 = WorkflowReview.new :article => a, :review_slot => slot
     assert (not r2.save)
     assert r2.errors[:review_slot]
+  end
+  test "article can have two reviews in different slots" do
+    news = Section.create! :name => "news", :priority => 10, :url => "news"
+    slot1 = ReviewSlot.create! :name => "Section Editor"
+    slot2 = ReviewSlot.create! :name => "Copy Editor"
+    
+    a = Article.create! :section => news, :name => "smiley 80s"
+    r1 = WorkflowReview.create! :article => a, :review_slot => slot1
+    
+    r2 = WorkflowReview.new :article => a, :review_slot => slot2
+    assert (r2.save)
   end
 end
