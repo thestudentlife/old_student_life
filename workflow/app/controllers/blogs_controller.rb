1555ca44d9c4ba6534377985865dd9bf4d585543
Michael Maltese <mike@mikemaltese.com>
2011-03-19 11:16:07 -0700

    Neither was BlogsController

--- workflow/app/controllers/blogs_controller.rb
+++ workflow/app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   
   layout "front"
   
-  before_filter do @most_viewed = ViewedArticle.latest_most_viewed(10) end
+  before_filter do @most_viewed = WebPublishedArticle.latest_most_viewed(10) end
   before_filter do @sections = Section.all end
   
   def index
