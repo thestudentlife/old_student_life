596f9b283c14eaa60314c74803ce59c932bc6df7
Michael Maltese <mike@mikemaltese.com>
2011-03-21 22:37:40 -0700

    Show articles in issue by section, sort by name

--- workflow/app/controllers/workflow/issues_controller.rb
+++ workflow/app/controllers/workflow/issues_controller.rb
@@ -2,6 +2,12 @@ class Workflow::IssuesController < WorkflowController
   inherit_resources
   
   def show
-    super { @articles = @issue.articles }
+    super do
+      @articles_by_section = {}
+      Section.all.each do |section|
+        @articles_by_section[section] = @issue.articles.where(:section_id => section.id).sort_by(&:name)
+      end
+      
+    end
   end
 end
