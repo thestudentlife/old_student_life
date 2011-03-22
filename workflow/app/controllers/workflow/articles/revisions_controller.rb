27770519114e8f551a9f090615aeec55db4523c5
Michael Maltese <mike@mikemaltese.com>
2011-03-22 16:42:03 -0700

    'Edit latest revision' button, fills textarea with text of latest revision

--- workflow/app/controllers/workflow/articles/revisions_controller.rb
+++ workflow/app/controllers/workflow/articles/revisions_controller.rb
@@ -1,7 +1,25 @@
+module ReverseMarkdown
+  def self.html_to_markdown (text)
+    text.
+    gsub(/<p>(.*?)<\/p>/) { "#$1\n\n" }.
+    gsub(/<strong>(.*?)<\/strong>/) { "**#$1**" }.
+    gsub(/<em>(.*?)<\/em>/) { "*#$1*" }.
+    strip
+  end
+end
+
 class Workflow::Articles::RevisionsController < WorkflowController
   inherit_resources
   belongs_to :article
   before_filter :require_user
+  
+  def new
+    new! do
+      if @article.revisions.latest.any?
+        @revision.body = ReverseMarkdown.html_to_markdown(@article.revisions.latest.first.body)
+      end
+    end
+  end
   
   def create
     params[:revision] ||= {}
