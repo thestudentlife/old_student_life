ad33e3b13d4466411fcdb89e24db9b1dbf273e29
Michael Maltese <mike@mikemaltese.com>
2011-03-23 00:02:52 -0700

    WebPublishedArticlesController takes 7 hours off of Time.now on action #new, to make it actually Time.now, rather than Time.now 7 hours in the future

--- workflow/app/controllers/workflow/articles/web_published_articles_controller.rb
+++ workflow/app/controllers/workflow/articles/web_published_articles_controller.rb
@@ -3,6 +3,12 @@ class Workflow::Articles::WebPublishedArticlesController < WorkflowController
   actions :new, :create
   belongs_to :article
   
+  def new
+    new! do
+      @web_published_article.published_at = Time.now - 7.hours
+    end
+  end
+  
   def create
     create! { workflow_article_path(@article) }
   end
