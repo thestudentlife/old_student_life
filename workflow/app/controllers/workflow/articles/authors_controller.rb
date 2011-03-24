52cb1d601c2149231bec1a683ee75ea7e7411fa4
Michael Maltese <mike@mikemaltese.com>
2011-03-23 21:03:41 -0700

    fixed in workflow/articles/autors#new

--- workflow/app/controllers/workflow/articles/authors_controller.rb
+++ workflow/app/controllers/workflow/articles/authors_controller.rb
@@ -4,7 +4,7 @@ class Workflow::Articles::AuthorsController < WorkflowController
   before_filter { @article = Article.find params[:article_id] }
   
   def new
-    respond_with :workflow, @authors = Author.all.alphabetical
+    respond_with :workflow, @authors = Author.alphabetical
   end
   def create
     @author = Author.find params[:author_id]
