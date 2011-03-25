6e25041de5ac01bf4c9d052d2866d3df837b0395
Michael Maltese <mike@mikemaltese.com>
2011-03-24 17:55:04 -0700

    Select section in navbar when on article page

--- workflow/app/helpers/articles_helper.rb
+++ workflow/app/helpers/articles_helper.rb
@@ -1,5 +1,6 @@
 module ArticlesHelper
   def section_is_selected? (section)
-    "selected" if request.fullpath.match /^\/articles\/#{section.url}/
+    "selected" if request.fullpath.match /^\/articles\/#{section.url}/ or
+      request.fullpath.match /^\/articles\/\d+\/\d+\/\d+\/#{section.url}/
   end
 end
