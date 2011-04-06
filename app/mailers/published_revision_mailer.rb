909dc4f2272d473586fd01e368acab8dc19381b9
Michael Maltese <mike@mikemaltese.com>
2011-04-06 14:47:34 -0700

    Removed extraneous mailer folder

--- app/mailers/published_revision_mailer.rb
+++
@@ -1,8 +0,0 @@
-class PublishedRevisionMailer < ActionMailer::Base
-  default :from => "from@example.com"
-  
-  def revision_email (revision)
-    attachments["#{revision.title}.incx"] = InCopy.html_to_incopy(revision.body)
-    mail(:to => "mchtly@gmail.com", :subject => revision.title )
-  end
-end
