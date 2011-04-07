d39b5d9ca7f8f52571ac469b4670ca487b18f64a
Michael Maltese <mike@mikemaltese.com>
2011-04-07 00:16:11 -0700

    Fix article#show on new articles (titles was blowing up)

--- app/controllers/workflow/articles_controller.rb
+++ app/controllers/workflow/articles_controller.rb
@@ -1,6 +1,12 @@
 class Workflow::ArticlesController < WorkflowController
   respond_to :html
-  before_filter { @article = Article.find params[:id] }
+  before_filter do
+    @article = Article.find params[:id]
+    if @article.titles.nil?
+      @article.titles = []
+      @article.save
+    end
+  end
   
   def show
     respond_with :workflow, @article
@@ -12,7 +18,6 @@ class Workflow::ArticlesController < WorkflowController
   def update
     workflow_update = WorkflowUpdate.new_for_article_params(@article, params[:article])
     workflow_update.author = current_user
-    
     
     if @article.update_attributes params[:article] and (workflow_update.updates.any? ? workflow_update.save : true)
       redirect_to workflow_article_path(@article), :notice => "Article was successfully updated"
