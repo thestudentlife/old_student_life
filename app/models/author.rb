7dc9663402c8d8f24ce45ab09c91aa0c334ab90f
Michael Maltese <mike@mikemaltese.com>
2011-04-11 15:53:12 -0700

    Search authors by name

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,10 @@ class Author < ActiveRecord::Base
   has_and_belongs_to_many :articles
   validates_presence_of :name
 
+  searchable do
+    text :name
+  end
+
   def self.alphabetical
     all.sort_by do |author|
       author.name.downcase
