af930daa3629a484e73b6f20df11103e120479ae
Michael Maltese <mike@mikemaltese.com>
2011-09-12 16:50:40 -0700

    Fixed PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < HighVoltage::PagesController
   layout "front"
   
-  before_filter do @most_viewed = WebPublishedArticle.latest_most_viewed(10) end
+  before_filter do @most_viewed = [] end
   before_filter do @sections = Section.all end
 end
