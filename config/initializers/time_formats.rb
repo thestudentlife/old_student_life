cb7c454d91b282d4a73e68e04b0ff08241fe5310
staceyabrams <stacey.r.abrams@gmail.com>
2014-02-23 22:20:21 -0800

    Refined timestamp to month, day, and year

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,6 @@
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Time::DATE_FORMATS[:pretty] = lambda do |time|
-  time.strftime("%a, %b %e, %Y at %l:%M") + time.strftime("%p").downcase
+  time.strftime("%b %e, %Y")
 end
 Time::DATE_FORMATS[:default] = Time::DATE_FORMATS[:pretty]
 
