2f54a11fd96250dfecf5e66fb52604d3d9ff9891
staceyabrams <stacey.r.abrams@gmail.com>
2014-02-23 21:51:21 -0800

    Added the year to article timestamps

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,6 @@
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Time::DATE_FORMATS[:pretty] = lambda do |time|
-  time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase
+  time.strftime("%a, %b %e, %Y at %l:%M") + time.strftime("%p").downcase
 end
 Time::DATE_FORMATS[:default] = Time::DATE_FORMATS[:pretty]
 
