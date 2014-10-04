31b5b28b85fc108bc8b5e37dbe5e4330e8a866ed
TSL Admin <tsladmin@TSL-mac03.local>
2014-10-04 16:44:44 -0700

    Manual extension of HighVoltage PageController.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,33 @@
-class PagesController < HighVoltage::PagesController
+class PagesController < ApplicationController
+
+  unloadable
+
   layout "front"
   
   before_filter do @most_viewed = [] end
   before_filter do @sections = Section.all end
+
+  rescue_from ActionView::MissingTemplate do |exception|
+    if exception.message =~ %r{Missing template pages/}
+      raise ActionController::RoutingError, "No such page: #{params[:id]}"
+    else
+      raise exception
+    end
+  end
+
+  def show
+    render :template => current_page
+  end
+
+  protected
+
+    def current_page
+      "pages/#{clean_path}"
+    end
+
+    def clean_path
+      path = Pathname.new "/#{params[:id]}"
+      path.cleanpath.to_s[1..-1]
+    end
+
 end
